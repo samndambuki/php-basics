b5e69d71db68aef6e259588382010ba4cf09a187
samndambuki <sam.ndambuki08@gmail.com>
2024-01-22 16:33:41 +0300

    initial commit

---
+++ 01_output.php
@@ -0,0 +1,31 @@
+<?php
+//outputs strings, numbers, html
+// echo 'Hello World',123,10.5;
+
+//print - works like echo but can only take a single argument
+// print 123;
+
+//print_r() - prints single values and arrays
+// print_r([1,2,3]);
+
+//var_dump() - returns more info like data type and length
+// var_dump("Hello");
+// var_dump(true);
+
+
+///var_export - outputs a string representation of a variable
+// var_export('Hello');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+    </title> 
+</head>
+<body>
+<h1><?php echo 'Post One'; ?> </h1>
+</body>
+</html>
