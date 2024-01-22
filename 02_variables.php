86a6b6eb4ee7f5cbe88055581c0ad4f222fa6a14
samndambuki <sam.ndambuki08@gmail.com>
2024-01-22 16:55:21 +0300

    variables

---
+++ 02_variables.php
@@ -0,0 +1,30 @@
+<?php
+$name = "Samuel";
+// echo $name;
+
+$age = 40;
+$has_kids = true;
+$cash_on_hand = 20.75;
+
+
+//bool
+// var_dump($has_kids);
+
+//flaot
+// var_dump($cash_on_hand);
+
+// echo $name . 'is' . $age . 'years old';
+
+// echo "${name} is ${age} years old";
+
+// $x =  5+5;
+// var_dump($x);
+
+
+//constants - variables that will not change
+define('HOST', 'localhost');
+define('DB_NAME','dev_db');
+
+echo HOST;
+
+?>
