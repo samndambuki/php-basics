2d52444798cd7ae389b1da77f39536ca210a8769
samndambuki <sam.ndambuki08@gmail.com>
2024-01-26 11:05:59 +0300

    worked with cookies

--- 12_cookies.php
+++ 12_cookies.php
@@ -1,1 +1,16 @@
-<!-- cookies - mechanism for storing data in a remote browser -->
+<!-- cookies - mechanism for storing data in a remote browser and retrieving it when the user visits the site again -->
+
+<?php
+
+//set cookie 
+setcookie('name','Sam',time()+86400 * 30);
+
+//get the cookiie and use it
+
+//check for the cookie
+if(isset($_COOKIE['name'])){
+    echo $_COOKIE['name'];
+}
+
+//delete cookie
+setcookie('name','',time()-86400);
