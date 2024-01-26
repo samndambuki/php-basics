8de9df267de6ce4743f8f11acb64d6402900de9b
samndambuki <sam.ndambuki08@gmail.com>
2024-01-26 09:32:49 +0300

    worked with string functions

---
+++ 08_string_functions.php
@@ -0,0 +1,49 @@
+<?php 
+$string = "Hello World";
+
+//get length of a string
+// echo strlen($string);
+
+//find first occurrence
+// echo strpos($string,'o');
+
+//find last occurence
+// echo strrpos($string,'o');
+
+//reverse 
+// echo strrev($string);
+
+//convert to lower case
+// echo strtolower($string);
+
+//convert to upper
+// echo strtoupper($string);
+
+//upper case first character of each word
+// echo ucwords($string);
+
+//string replace
+// echo str_replace('World','Everyone',$string);
+
+
+//return a portion of the string specified by the offset and length
+// echo substr($string,0,5);
+
+// echo substr($string,5);
+
+// if(str_starts_with($string,'Hello')){
+//     echo 'YES';
+// }
+
+//ends with
+// if(str_ends_with($string,'ld')){
+//     echo 'YES';
+// }
+
+// $string2 = '<h1>Hello</h1>';
+$string2 = '<script>alert(1)</script>';
+// echo htmlspecialchars($string2);
+
+// printf('%s likes to %s','Brad','Code');
+
+printf('1+1=%f',1+1);
