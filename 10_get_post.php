af894e88afefe2b989ff5a6a42796debf92744a4
samndambuki <sam.ndambuki08@gmail.com>
2024-01-26 10:03:49 +0300

    worked with get and post

--- 10_get_post.php
+++ 10_get_post.php
@@ -1,6 +1,27 @@
 <?php
 
+echo $_POST['name'];
+echo $_POST['age'];
 ?>
 
 <!-- PHP_SELF - the file we are in -->
-<a href="<?php echo $_SERVER['PHP_SELF']; ?>">click</a>
+<a href="<?php echo $_SERVER['PHP_SELF']; ?>?name=Brad&age=30">click</a>
+
+
+<!-- action="" - the form we want to submit to -->
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST"> 
+
+<div>
+    <label for="name">Name:</label>
+    <input type="text" name="name">
+</div>
+
+<div>
+    <label for="age">Age:</label>
+    <input type="text" name="age">
+</div>
+
+<input type="submit" value="submit" name="submit">
+
+</form>
+
