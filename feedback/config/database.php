36ca7cee0f4e2352362dac57432bdfab9f30a54c
samndambuki <sam.ndambuki08@gmail.com>
2024-01-29 14:43:00 +0300

    configured the database

---
+++ feedback/config/database.php
@@ -0,0 +1,16 @@
+<?php
+define('DB_HOST','localhost');
+define('DB_USER','samuel');
+define('DB_PASS','123456');
+define('DB_NAME','php_dev');
+
+//create connection
+$conn = new mysqli(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+//check the connection
+if($conn->connect_error){
+    die('Connection failed' . $conn->connect_error);
+}
+
+
+echo 'CONNECTED!';
